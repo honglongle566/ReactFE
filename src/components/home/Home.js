d43a338bdf25611b1f0f16af8658c3e7160077aa
Hong Long <64268379+honglongle566@users.noreply.github.com>
2022-03-04 09:49:39 +0700

    Update Home.js

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,12 +7,6 @@ export default class Home extends React.Component {
     render(){
         return (
             <div className="Home" style={{display: "block"}}>
-                {/*
-                <div className="welcome">Chào mừng đến với hệ thống đồng bộ lịch biểu
-                <br/>
-                    Nhóm 2 - K63T - Tích hợp hệ thống
-                </div>
-                */}
                 <BrowserRouter>
                     <Tabs/>
                 </BrowserRouter>
